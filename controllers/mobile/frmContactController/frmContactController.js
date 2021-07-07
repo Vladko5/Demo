3ce570b64d5ced65718430f53158f331544b19ea
Vladko5 <vladi_dimitrovv@abv.bg>
2021-07-07 15:41:06 +0300

    commit to 'master'

--- controllers/mobile/frmContactController/frmContactController.js
+++ controllers/mobile/frmContactController/frmContactController.js
@@ -5,7 +5,9 @@ define({
   },
   
   onNavigate: function(contactData){
-    this.view.lblContacts.onTouchStart = this.onTouchStart;
+    this.view.lblContacts.onTouchStart = this.backToContacts;
+    this.view.lblEdit.onTouchStart = this.editDetails;
+    this.view.imgEditDone.onTouchStart = this.finishEditing;
 
     this.view.lblName.text = contactData.name ? contactData.name : "N/A";
 	this.view.imgContact.src = contactData.img ? contactData.img : "option3.png";
@@ -14,9 +16,29 @@ define({
     this.view.lblDetails.text = contactData.notes ? contactData.notes : "N/A";
   },
   
-  onTouchStart: function(){
+  backToContacts: function(){
     const ntf = new kony.mvc.Navigation("frmSegList");
     ntf.navigate();
+  },
+  
+  editDetails: function() {
+    let editMobileBx = this.view.txtBxEditMobile;
+    let mobileBx = this.view.lblMobileNum;
+    editMobileBx.text = mobileBx.text;
+    editMobileBx.isVisible = true;
+    mobileBx.isVisible = false;
+    this.view.lblEdit.isVisible = false;
+    this.view.imgEditDone.isVisible = true;
+  },
+  
+  finishEditing: function() {
+    let editMobileBx = this.view.txtBxEditMobile;
+    let mobileBx = this.view.lblMobileNum;
+    mobileBx.text = editMobileBx.text;
+    editMobileBx.isVisible = false;
+    mobileBx.isVisible = true;
+    this.view.lblEdit.isVisible = true;
+    this.view.imgEditDone.isVisible = false;
   }
 
  });
